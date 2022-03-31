952a16d977c2c3378700539bd4a3781d4e029e3f
elmurod <elmurodzulfiqarov@gmail.com>
2022-03-31 23:17:44 +0500

    add .gitignore

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import telebot
 from transliterate import to_cyrillic, to_latin
 
-TOKEN = ""
+TOKEN = "Set yout token"
 bot = telebot.TeleBot(token=TOKEN)
 
 @bot.message_handler(commands=["start"])
